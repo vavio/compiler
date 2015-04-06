e27cbefc643075dc0236454116e4f1f9775f91a6
Valentin Ambaroski <vavio.valentin@gmail.com>
2015-04-06 23:10:24 +0200

    UTF-8 writing
    Updated to write to the file with UTF-8 encoding

--- src/mk/edu/finki/compiler/lexer/Test.java
+++ src/mk/edu/finki/compiler/lexer/Test.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,8 +20,8 @@ public class Test {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				new FileInputStream("source.robo"), "UTF8"));
-		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
-				"tokens")));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream("tokens"), "UTF-8"));
 		Lexer lexer = new Lexer(in);
 
 		Token token = null;
@@ -32,15 +34,15 @@ public class Test {
 				out.write(String.format("(%d) l=%d c=%d\n",
 						hashTokens.get(lexer.leksema), lexer.line, lexer.column));
 			} else {
-				out.write(String.format(" l=%d c=%d\n", lexer.line, lexer.column));
+				out.write(String.format(" l=%d c=%d\n", lexer.line,
+						lexer.column));
 			}
 		}
 
 		out.write("\nID TABLE\n");
 		for (Map.Entry<String, Integer> iter : hashTokens.entrySet())
-			out.write(String.format("%d %s\n", iter.getValue(),
-					iter.getKey()));
-		
+			out.write(String.format("%d %s\n", iter.getValue(), iter.getKey()));
+
 		out.close();
 	}
 }
